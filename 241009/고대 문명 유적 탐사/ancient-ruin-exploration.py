4f08e3ee8e043b733921df8068b6b5d0ad4b84ea
Hyojin <77480872+HyojinPark1223@users.noreply.github.com>
2024-10-09 17:11:43 +0900

    Title: 고대 문명 유적 탐사, Time: 271ms, Memory: 27MB, Status: Wrong Answer - CodeTree

--- 241009/고대 문명 유적 탐사/ancient-ruin-exploration.py
+++ 241009/고대 문명 유적 탐사/ancient-ruin-exploration.py
@@ -1,203 +1,98 @@
-import pprint
-
 from collections import deque
+from copy import deepcopy
 
-def print_arr(arrs):
-    for i in range(len(arrs)):
-        for j in range(len(arrs)):
-            print(arrs[i][j], end=" ")
-        print()
+K, M = map(int, input().split())
+g = [list(map(int,input().split())) for _ in range(5)]
 
+treasure = deque(list(map(int, input().split())))
 
-def arr_copy(arr1, arr2):
-    for i in range(len(arr1)):
-        for j in range(len(arr1[0])):
-            arr1[i][j] = arr2[i][j]
+class Board:
+    def __init__(self):
+        self.b = [[0] * 5 for _ in range(5)]
 
+    def in_range(self, x, y):
+        return 0 <= x < 5 and 0 <= y < 5
 
-def rotate(sx, sy, rr):
-    tmp = [[0] * 3 for _ in range(3)]
-    result = [[0] * 3 for _ in range(3)]
+    def rotate(self, sx, sy, cnt):
+        result = Board()
+        result.b = deepcopy(self.b)
 
-    len_tmp = 3
+        if cnt == 0:
+            for i in range(3):
+                for j in range(3):
+                    result.b[sx + i][sy + j] = self.b[sx + 2 - j][sy + i]
+        elif cnt == 1:
+            for i in range(3):
+                for j in range(3):
+                    result.b[sx + i][sy + j] = self.b[sx + 2 - i][sy + 2 - j]
+        elif cnt == 2:
+            for i in range(3):
+                for j in range(3):
+                    result.b[sx + i][sy + j] = self.b[sx + j][sy + 2 - i]
 
-    for i in range(3):
-        for j in range(3):
-            tmp[i][j] = arr[sx + i][sy + j]
+        return result
 
-    for i in range(len_tmp):
-        for j in range(len_tmp):
-            if rr == 0:
-                result[j][len_tmp - i - 1] = tmp[i][j]  # 시계
-            elif rr == 1:
-                result[len_tmp - i - 1][len_tmp - j - 1] = tmp[i][j]  # 시계 180
-            else:
-                result[len_tmp - j - 1][i] = tmp[i][j]  # 시계 270
+    def get_score(self):
+        score = 0
+        visit = [[False] * 5 for _ in range(5)]
+        dx, dy = [1, 0, -1, 0], [0, 1, 0, -1]
 
-    for i in range(len_tmp):
-        for j in range(len_tmp):
-            arr[i + sx][j + sy] = result[i][j]
+        for i in range(5):
+            for j in range(5):
+                if not visit[i][j]:
+                    q = deque([(i, j)])
+                    trace = deque([(i, j)])
 
+                    visit[i][j] = True
+                    while q:
+                        cur = q.popleft()
+                        for k in range(4):
+                            nx, ny = cur[0] + dx[k], cur[1] + dy[k]
+                            if self.in_range(nx, ny) and self.b[nx][ny] == self.b[cur[0]][cur[1]] and not visit[nx][ny]:
+                                q.append((nx, ny))
+                                trace.append((nx, ny))
+                                visit[nx][ny] = True
 
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
+                    if len(trace) >= 3:
+                        score += len(trace)
+                        while trace:
+                            t = trace.popleft()
+                            self.b[t[0]][t[1]] = 0
+        return score
 
+    def fill(self, q):
+        for i in range(5):
+            for j in reversed(range(5)):
+                if self.b[j][i] == 0:
+                    self.b[j][i] = q.popleft()
 
-def bfs(x, y, c):
-    global nn
-    v = [[False] * nn for _ in range(nn)]
-    q = deque()
-    q.append([x, y, c])
-    v[x][y] = True
+board = Board()
+board.b = deepcopy(g)
 
-    selected = []
-    selected.append([x, y])
+for _ in range(K):
+    maxScore = 0
+    maxScoreBoard = None
 
-    while len(q) != 0:
-        px, py, pc = q.popleft()
+    for cnt in range(4):
+        for sx in range(3):
+            for sy in range(3):
+                rotated = board.rotate(sx, sy, cnt)
+                score = rotated.get_score()
 
-        for d in range(4):
-            nx = px + dx[d]
-            ny = py + dy[d]
-
-            if nx < 0 or nx >= nn or ny < 0 or ny >= nn or v[nx][ny] == True or arr[nx][ny] != pc:
-                continue
-
-            q.append([nx, ny, pc])
-            selected.append([nx, ny])
-            v[nx][ny] = True
-
-    if len(selected) >= 3:
-        for ii, jj in selected:
-            arr[ii][jj] = 0
-        return len(selected)
-
-    return 0
-
-
-def get_value():
-    global nn
-    gval = 0
-    for i in range(nn):
-        for j in range(nn):
-            if arr[i][j] != 0:
-                gval += bfs(i, j, arr[i][j])
-
-    return gval
-
-
-def fill():
-    global ww, nn
-
-    for j in range(nn):
-        for i in range(nn - 1, -1, -1):
-            if arr[i][j] == 0:
-                arr[i][j] = wall[ww]
-                ww += 1
-
-
-def get_real_value():
-    global ans
+                if maxScore < score:
+                    maxScore = score
+                    maxScoreBoard = rotated
+    if maxScoreBoard is None:
+        break
+    board = maxScoreBoard
 
     while True:
-        now_ans = 0
-        for i in range(nn):
-            for j in range(nn):
-                if arr[i][j] != 0:
-                    now_ans += bfs(i, j, arr[i][j])
-        if now_ans == 0:
+        board.fill(treasure)
+        newScore = board.get_score()
+        if newScore == 0:
             break
-        else:
-            # 정답 갱신
-            ans += now_ans
+        maxScore += newScore
 
-            # 유물 채우기
-            fill()
+    print(maxScore, end=" ")
 
-
-def pick():
-    global nn
-    pval = 0
-    px = -1
-    py = -1
-    pr = -1
-    for i in range(3):
-        for j in range(3):
-            for r in range(3):
-                arr_copy(arr, ori_arr)
-
-                # 이번에 찾을 위치 회전 시키기
-                rotate(i, j, r)
-
-                # 회전 시킨 유물의 1차 획득 가치 확인
-                gval = get_value()
-
-                if gval == 0:
-                    continue
-
-                if pval < gval:
-                    pval = gval
-                    px = i
-                    py = j
-                    pr = r
-                elif pval == gval:
-                    if pr > r:
-                        px = i
-                        py = j
-                        pr = r
-                    elif pr == r:
-                        if py > j:
-                            px = i
-                            py = j
-                            pr = r
-                        elif py == j:
-                            if px > i:
-                                px = i
-                                py = j
-                                pr = r
-
-    return px, py, pr
-
-
-kk, mm = list(map(int, input().split()))
-
-nn = 5
-
-ori_arr = []
-arr = [[0] * nn for _ in range(nn)]
-v = [[False] * nn for _ in range(nn)]
-
-for i in range(nn):
-    ori_arr.append(list(map(int, input().split())))
-
-wall = list(map(int, input().split()))
-ww = 0
-
-# 횟수만큼 반복
-ss = 0
-while ss < kk:
-    # 회전 목표 선택
-    xx, yy, rr = pick()
-
-    # 회전해서 없을 수 있는 값이 없으면 끝내기
-    if xx == -1:
-        break
-
-    # 회전
-    arr_copy(arr, ori_arr)
-    rotate(xx, yy, rr)
-
-    # 끝까지 유물 획득
-    ans = 0
-    get_real_value()
-
-    # 이번 턴에 획득한 유물이 없으면 끝내기
-    if ans == 0:
-        break
-
-    # 출력
-    print(ans, end=" ")
-
-    # 원본 배열 수정
-    arr_copy(ori_arr, arr)
-    ss += 1
+#print('{} {}'.format(K, M))
